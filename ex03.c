9d69199ff343234c938838d4aefcc231a513c3a2
Yohan Vallier <162353277+YohanVallier@users.noreply.github.com>
2024-04-27 15:08:42 -0300

    alteração

--- ex03.c
+++ ex03.c
@@ -3,18 +3,22 @@
 
 int main(){
     printf("%s\n", texto);
+
     int idade = 0;
     float altura = 0.0;
     char nome[50] = "";
+    
     printf("Digite a idade: ");
     scanf("%d", &idade);
+
     printf("Digite a altura: ");
     scanf("%f", &altura);
+
     printf("Digite o seu nome: ");
     scanf("%s", &nome);
     
     printf("Dados informados\n");
-    printf("Sua idade é : %d\n", &idade);
-    printf("Sua altura é: %f\n", &altura);
-    printf("Seu nome é: %s\n", &nome);
+    printf("Sua idade é : %d\n", idade);
+    printf("Sua altura é: %.2f\n", altura);
+    printf("Seu nome é: %s\n", nome);
 }
